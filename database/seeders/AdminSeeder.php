a15f7c32a39ee6a2878c6a93786a1fea02a9a6e1
Md. Abir Hasan <abirhasanshuvo789@gmail.com>
2026-09-20 22:02:49 +0600

    added admin as an user to the table and also in the web

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -20,7 +20,7 @@ class AdminSeeder extends Seeder
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => Hash::make('password'),
             'remember_token' => Str::random(10),
         ]
         );
